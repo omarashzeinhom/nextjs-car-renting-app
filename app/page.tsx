b226154c88a52d6314548851979f40624c591fcb
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-07-23 23:11:25 +0300

    UPDATED ./app/layout.tsxz

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Hero, Navbar, Footer } from "@/components";
-import Image from "next/image";
+import { Hero } from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Navbar />
       <Hero />
-      <Footer />
     </main>
   );
 }
