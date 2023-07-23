5fbc7e594164aad637953bf3a6570bfcd435521b
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-07-24 00:31:28 +0300

    ADDED SearchBar.tsx and SearchManufacturer.tsx to ./components

--- components/index.tsx
+++ components/index.tsx
@@ -2,4 +2,8 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Footer from './Footer';
 import Navbar from './Navbar';
-export {Hero, CustomButton,Footer, Navbar};
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+
+export {Hero, CustomButton,Footer, Navbar,SearchBar,CustomFilter,SearchManufacturer};
